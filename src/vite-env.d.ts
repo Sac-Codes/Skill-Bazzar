cdd034f5a4c62f75756974e159519d8c4764c944
Sac-Codes <sachin.codes.dev@gmail.com>
2026-07-23 06:21:03 +0530

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.JPG";
+declare module "*.svg";
+declare module "*.webp";
+declare module "*.gif";
+declare module "*.avif";
+
